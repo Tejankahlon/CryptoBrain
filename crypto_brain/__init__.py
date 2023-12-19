c781866ec02ffc332155d13f57c376a1363ca90a
Tejankahlon <tejankahlon56@gmail.com>
2023-12-19 14:41:39 -0800

    Initial commit

---
+++ crypto_brain/__init__.py
@@ -0,0 +1,8 @@
+
+from flask import Flask, session
+
+app = Flask(__name__)
+
+app.secret_key = "YOUR SECRET KEY"
+
+#TODO Change secret key
---
+++ server.py
@@ -0,0 +1,8 @@
+
+from my_app.controllers import #TODO controllers go here
+
+from my_app import app
+
+if __name__=="__main__":
+
+	app.run(debug=True)
